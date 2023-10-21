857c941d1f290b17e14be77da516a5e39b0d2699
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-10-21 18:22:43 +0200

    feat(drivingapp): v.1.0.0

--- DrivingApp/Car.fs
+++ DrivingApp/Car.fs
@@ -1,7 +1,17 @@
 ﻿module Car
 
+type DriveResult = {
+    GasLeft : float
+    IsOutOfGas : bool
+}
+
 let drive distance gas =
-    if distance > 50 then gas / 2.0
-    elif distance > 25 then gas - 10.0
-    elif distance > 0 then gas - 1.0
-    else gas
+    let output = 
+        if distance > 50 then gas / 2.0
+        elif distance > 25 then gas - 10.0
+        elif distance > 0 then gas - 1.0
+        else gas
+    {
+        GasLeft = output
+        IsOutOfGas = output <= 0.0 
+    }
--- DrivingApp/Program.fs
+++ DrivingApp/Program.fs
@@ -1,5 +1,12 @@
-﻿printfn "Hello Driving App"
+﻿open System
 
-let remaining = Car.drive 2 8
-printfn $"You have {remaining} gas left."
+printfn "Hello Driving App"
 
+printfn "How far do you want to drive?"
+let parsed, distance = Console.ReadLine() |> Int32.TryParse
+
+let startGas = 8.0
+let remainingGas = startGas |> Car.drive distance
+
+if remainingGas.IsOutOfGas then printfn "You are out of gas!"
+else printfn $"You have {remainingGas.GasLeft} gas left."
