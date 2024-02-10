f92a431c5b8f7a5e379d777805e996a228c1864b
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-02-10 21:00:42 +0100

    feat(scripts): customer.fsx refactor

--- scripts/customer.fsx
+++ scripts/customer.fsx
@@ -1,25 +1,20 @@
-type RegisteredCustomer = { Id: string; IsEligible: bool }
-
-type UnregisteredCustomer = { Id: string }
 
 type Customer =
-    | Registered of RegisteredCustomer
-    | Guest of UnregisteredCustomer
+    | Registered of Id: string * IsEligible: bool
+    | Guest of Id: string
     
-let sarah = Guest { Id = "Sarah" }
-let john = Registered { Id = "John"; IsEligible = true }
-let mary = Registered { Id = "Mary"; IsEligible = true }
-let richard = Registered { Id = "Richard"; IsEligible = false }
+let john = Registered (Id = "John", IsEligible = true)
+let mary = Registered (Id = "Mary", IsEligible = true)
+let richard = Registered (Id = "Richard", IsEligible = false)
+let sarah = Guest (Id = "Sarah")
 
 let calculateTotal customer spend =
     let discount =
         match customer with
-        | Registered c -> if c.IsEligible && spend >= 100.0m then spend * 0.1m else 0.0m
-        | Guest _ -> 0.0m
+        | Registered (IsEligible = true) when spend >= 100.0m -> spend * 0.1m
+        | _ -> 0.0m
     
     spend - discount
-
-
 
 let areEqual expected actual =
     actual = expected
