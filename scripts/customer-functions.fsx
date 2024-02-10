d54a9ef71cdf3278f06e4ae97ed0d35990146010
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-02-10 21:57:02 +0100

    feat(scripts) customer functional composing

---
+++ scripts/customer-functions.fsx
@@ -0,0 +1,49 @@
+﻿type Customer = { Id: int; IsVip: bool; Credit: decimal }
+
+let getPurchases customer =
+    let purchases = if customer.Id % 2 = 0 then 120m else 80m
+    customer, purchases
+
+let tryPromoteVip purchases =
+    let customer, amount = purchases
+    if amount > 100m then { customer with IsVip = true } else customer
+
+let increaseCreditIfVip customer =
+    let increase = if customer.IsVip then 100m else 50m
+    { customer with Credit = customer.Credit + increase }
+
+// Composition operator
+let upgradeCustomerComposed =
+    getPurchases >> tryPromoteVip >> increaseCreditIfVip
+
+// Nested
+let upgradeCustomerNested customer =
+    increaseCreditIfVip (tryPromoteVip (getPurchases customer))
+
+// Procedural
+let upgradeCustomerProcedural customer =
+    let customerWithPurchases = getPurchases customer
+    let promotedCustomer = tryPromoteVip customerWithPurchases
+    let increasedCreditCustomer = increaseCreditIfVip promotedCustomer
+    increasedCreditCustomer
+
+// Forward pipe operator
+let upgradeCustomerPiped customer =
+    customer |> getPurchases |> tryPromoteVip |> increaseCreditIfVip
+
+
+let customerVIP = { Id = 1; IsVip = true; Credit = 0m }
+let customerSTD = { Id = 2; IsVip = false; Credit = 100m }
+
+let assertVIP =
+    upgradeCustomerComposed customerVIP = { Id = 1; IsVip = true; Credit = 100m }
+
+let assertSTDtoVIP =
+    upgradeCustomerComposed customerSTD = { Id = 2; IsVip = true; Credit = 200m }
+
+let assertSTD =
+    upgradeCustomerComposed { customerSTD with Id = 3; Credit = 50m } = { Id = 3; IsVip = false; Credit = 100m }
+    
+printfn $"{assertVIP}"
+printfn $"{assertSTDtoVIP}"
+printfn $"{assertSTD}"
--- scripts/customer.fsx
+++ scripts/customer.fsx
@@ -3,11 +3,6 @@ type Customer =
     | Registered of Id: string * IsEligible: bool
     | Guest of Id: string
     
-let john = Registered (Id = "John", IsEligible = true)
-let mary = Registered (Id = "Mary", IsEligible = true)
-let richard = Registered (Id = "Richard", IsEligible = false)
-let sarah = Guest (Id = "Sarah")
-
 let (|IsEligible|_|) customer =
     match customer with
     | Registered (IsEligible = true) -> Some ()
@@ -23,6 +18,11 @@ let calculateTotal customer spend =
 
 let areEqual expected actual =
     actual = expected
+
+let john = Registered (Id = "John", IsEligible = true)
+let mary = Registered (Id = "Mary", IsEligible = true)
+let richard = Registered (Id = "Richard", IsEligible = false)
+let sarah = Guest (Id = "Sarah")
 
 let assertJohn = areEqual 90.0m (calculateTotal john 100.0m)
 let assertMary = areEqual 99.0m (calculateTotal mary 99.0m)
