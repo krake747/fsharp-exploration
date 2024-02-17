68a2bef5ac4544d9f7983b8fd771025a8c3e5047
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-02-17 13:51:03 +0100

    feat(fsharp): validation problem

--- src/MyConsole/Program.fs
+++ src/MyConsole/Program.fs
@@ -1,4 +1,30 @@
-﻿open System.IO
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+type Customer = {
+    CustomerId: string
+    Email: string
+    IsEligible: string
+    IsRegistered: string
+    DateRegistered: string
+    Discount: string
+}
+
+type ValidatedCustomer = {
+    CustomerId: string
+    Email: string option
+    IsEligible: bool
+    IsRegistered: bool
+    DateRegistered: DateTime option
+    Discount: decimal option
+}
+
+type ValidationError =
+    | MissingData of name: string
+    | InvalidData of name: string * value: string
+
+type FileReader = string -> Result<string seq, exn>
 
 // exception will not get caught because seq {...} is lazily evaluated
 // let readFile path =
@@ -12,18 +38,7 @@
 //     with
 //     | ex -> Error ex
 
-type Customer = {
-    CustomerId: string
-    Email: string
-    IsEligible: string
-    IsRegistered: string
-    DateRegistered: string
-    Discount: string
-}
-
-type DataReader = string -> Result<string seq, exn>
-
-let readFile: DataReader =
+let readFile: FileReader =
     fun path ->
         try
             File.ReadLines(path) |> Ok
@@ -43,19 +58,125 @@ let parseLine (line: string) : Customer option =
         }
     | _ -> None
 
+let (|ParseRegex|_|) (regex: string) (str: string) =
+    let m = Regex.Match(str, regex)
+    if m.Success then Some(List.tail [ for x in m.Groups -> x.Value ]) else None
+
+let (|IsValidEmail|_|) (input: string) =
+    match input with
+    | ParseRegex ".*?@(.*)" [ _ ] -> Some input
+    | _ -> None
+
+let (|IsEmptyString|_|) (input: string) =
+    if input.Trim() = "" then Some() else None
+
+let (|IsDecimal|_|) (input: string) =
+    let success, value = Decimal.TryParse input
+    if success then Some value else None
+
+let (|IsBoolean|_|) (input: string) =
+    match input with
+    | "1" -> Some true
+    | "0" -> Some false
+    | _ -> None
+
+let (|IsValidDate|_|) (input: string) =
+    let success, value = input |> DateTime.TryParse
+    if success then Some value else None
+
+let validateCustomerId (customerId: string) =
+    if customerId <> "" then Ok customerId else Error(MissingData "CustomerId")
+
+let validateEmail (email: string) =
+    if email <> "" then
+        match email with
+        | IsValidEmail _ -> Ok(Some email)
+        | _ -> Error(InvalidData("Email", email))
+    else
+        Ok None
+
+let validateIsEligible (isEligible: string) =
+    match isEligible with
+    | IsBoolean b -> Ok b
+    | _ -> Error(InvalidData("IsEligible", isEligible))
+
+let validateIsRegistered (isRegistered: string) =
+    match isRegistered with
+    | IsBoolean b -> Ok b
+    | _ -> Error(InvalidData("IsRegistered", isRegistered))
+
+let validateDateRegistered (dateRegistered: string) =
+    match dateRegistered with
+    | IsEmptyString -> Ok None
+    | IsValidDate dt -> Ok(Some dt)
+    | _ -> Error(InvalidData("DateRegistered", dateRegistered))
+
+let validateDiscount (discount: string) =
+    match discount with
+    | IsEmptyString -> Ok None
+    | IsDecimal value -> Ok(Some value)
+    | _ -> Error(InvalidData("Discount", discount))
+
+let getError input =
+    match input with
+    | Ok _ -> []
+    | Error ex -> [ ex ]
+
+let getValue input =
+    match input with
+    | Ok v -> v
+    | _ -> failwith "Oops, you shouldn't have got here!"
+
+let create customerId email isEligible isRegistered dateRegistered discount = {
+    CustomerId = customerId
+    Email = email
+    IsEligible = isEligible
+    IsRegistered = isRegistered
+    DateRegistered = dateRegistered
+    Discount = discount
+}
+
+let validate (input: Customer) : Result<ValidatedCustomer, ValidationError list> =
+    let customerId = input.CustomerId |> validateCustomerId
+    let email = input.Email |> validateEmail
+    let isEligible = input.IsEligible |> validateIsEligible
+    let isRegistered = input.IsRegistered |> validateIsRegistered
+    let dateRegistered = input.DateRegistered |> validateDateRegistered
+    let discount = input.Discount |> validateDiscount
+
+    let errors =
+        [
+            customerId |> getError
+            email |> getError
+            isEligible |> getError
+            isRegistered |> getError
+            dateRegistered |> getError
+            discount |> getError
+        ]
+        |> List.concat
+
+    match errors with
+    | [] ->
+        Ok(
+            create
+                (customerId |> getValue)
+                (email |> getValue)
+                (isEligible |> getValue)
+                (isRegistered |> getValue)
+                (dateRegistered |> getValue)
+                (discount |> getValue)
+        )
+    | _ -> Error errors
+
 let parse (data: string seq) =
-    data
-    |> Seq.skip 1 // Ignore header row
-    |> Seq.map parseLine
-    |> Seq.choose id // Ignore None and unwrap Some
+    data |> Seq.skip 1 |> Seq.map parseLine |> Seq.choose id |> Seq.map validate
 
-let output data =
-    data |> Seq.iter (fun x -> printfn $"{x}")
+let output data = data |> Seq.iter (printfn "%A")
 
-let import (dataReader: DataReader) path =
-    match path |> dataReader with
+let import (fileReader: FileReader) path =
+    match path |> fileReader with
     | Ok data -> data |> parse |> output
-    | Error ex -> printfn $"Error: %A{ex.Message}"
+    | Error ex -> printfn $"Error: %A{ex}"
 
 let importWithFileReader = import readFile
 
@@ -63,14 +184,15 @@ let importWithFileReader = import readFile
 Path.Combine(__SOURCE_DIRECTORY__, "resources", "customers.csv")
 |> importWithFileReader
 
-let fakeDataReader : DataReader =
-    fun _ ->
-        seq {
-            "CustomerId|Email|Eligible|Registered|DateRegistered|Discount"
-            "John|john@test.com|1|1|2015-01-23|0.1"
-            "Mary|mary@test.com|1|1|2018-12-12|0.1"
-            "Richard|richard@nottest.com|0|1|2016-03-23|0.0"
-            "Sarah||0|0||"
-        } |> Ok
-        
-import fakeDataReader "_"
+// let fakeDataReader: FileReader =
+//     fun _ ->
+//         seq {
+//             "CustomerId|Email|Eligible|Registered|DateRegistered|Discount"
+//             "John|john@test.com|1|1|2015-01-23|0.1"
+//             "Mary|mary@test.com|1|1|2018-12-12|0.1"
+//             "Richard|richard@nottest.com|0|1|2016-03-23|0.0"
+//             "Sarah||0|0||"
+//         }
+//         |> Ok
+//
+// import fakeDataReader "_"
